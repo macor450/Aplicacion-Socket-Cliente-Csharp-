ad8393c77b7c664bb20b172bd7adbfb4e95646f9
Martin Cortez <i20050008@monclova.tecnm.mx>
2023-10-16 19:59:06 -0600

    Initial commit

---
+++ Aplicacion Socket Cliente(Csharp)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AplicaciondeSocketCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEp = new IPEndPoint(ipAddress, 11200);
+            try
+            {
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                sender.Connect(remoteEp);
+
+                Console.WriteLine("Conectando con el  servidor");
+                Console.WriteLine("Ingrese un texto que quiere enviar");
+                string texto = Console.ReadLine();
+
+                byte[] msg = Encoding.ASCII.GetBytes(texto + "<EOF>");
+                int byteSent = sender.Send(msg);
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception e)
+            { Console.WriteLine(e.ToString()); }
+        }
+
+
+    }
+}
